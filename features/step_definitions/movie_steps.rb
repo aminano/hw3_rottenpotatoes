bfe6037faefa8a8c17f0c3f390cb644b3e2005f4
Private User <alx@BlackHole.(none)>
2012-10-21 14:01:59 +0200

    HW3 parts 1 and 2

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,4 +25,37 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(", ")
+  ratings.each do |rating|
+    if uncheck
+      uncheck "ratings_#{rating}"
+    else
+      check "ratings_#{rating}"
+    end
+  end
 end
+
+Then /I should(n't)? see the following movies: (.*)/ do |negative, titles_list|
+  titles = titles_list.split(", ")
+  titles.each do |title|
+    if negative
+      assert page.has_content?(title) == false
+    else
+      assert page.has_content?(title)
+    end
+  end
+end
+
+When /^I press (.*)$/ do |clicked|
+  click_button clicked
+end
+
+Then /I should see (all|none) of the movies/ do |which|
+  if which == "all"
+    size_db = Movie.all.size
+  else
+    size_db = 0
+  end
+  assert page.all("table#movies tbody tr").count == size_db
+end
+
